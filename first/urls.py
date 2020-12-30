f30bfaa9cf6f2b237378966ef13676b144137404
kiteB <69155170+kiteB@users.noreply.github.com>
2020-12-30 19:06:23 +0900

    템플릿 기본 (변수 넘기기)

--- first/urls.py
+++ first/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('select/', views.select, name="select"),
-    path('result/', views.result, name="result"),
+    path('', views.index, name='index'),
+    path('select/', views.select, name='select'),
+    path('result/', views.result, name='result'),
 ]
