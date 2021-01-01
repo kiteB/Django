7820dbe4c8b1fd9e27565fb44df268886e88bf50
kiteB <69155170+kiteB@users.noreply.github.com>
2021-01-01 21:51:50 +0900

    D: 게시글 삭제 구현하기

--- third/views.py
+++ third/views.py
@@ -51,3 +51,10 @@ def detail(request):
         item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
         return render(request, 'third/detail.html', {'item': item})
     return HttpResponseRedirect('/third/list/')
+
+
+def delete(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        item.delete()
+    return HttpResponseRedirect('/third/list/')
