061852b7f67026e1468050bc6d3de00a2dee0ed8
kiteB <69155170+kiteB@users.noreply.github.com>
2021-01-01 19:50:12 +0900

    게시글 등록 구현하기

--- third/urls.py
+++ third/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('list/', views.list, name="list/"),
-}
+    path('create/', views.create, name="restaurant-create"),
+]
